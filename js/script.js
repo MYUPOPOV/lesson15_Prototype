6670fdc65f5363c9ed5654c3a26e2c3e0c8b3d25
MYuPopov <beemax@bk.ru>
2021-12-16 23:04:00 +0300

    Усложненное задание 15

--- js/script.js
+++ js/script.js
@@ -1,5 +1,46 @@
 "use strict";
 
+window.addEventListener("keydown", (event) => {
+	if (event.key === "ArrowUp") {
+		goUp();
+	}
+	if (event.key === "ArrowDown") {
+		goDown();
+	}
+	if (event.key === "ArrowLeft") {
+		goLeft();
+	}
+	if (event.key === "ArrowRight") {
+		goRight();
+	}
+
+	// console.log(event);
+});
+
+const goUp = () => {
+	let positionY = newDivComplex.newElement.style.top;
+	positionY = parseInt(positionY) - 10;
+	newDivComplex.newElement.style.top = positionY + "px";
+};
+
+const goDown = () => {
+	let positionY = newDivComplex.newElement.style.top;
+	positionY = parseInt(positionY) + 10;
+	newDivComplex.newElement.style.top = positionY + "px";
+};
+
+const goRight = () => {
+	let positionX = newDivComplex.newElement.style.left;
+	positionX = parseInt(positionX) + 10;
+	newDivComplex.newElement.style.left = positionX + "px";
+};
+
+const goLeft = () => {
+	let positionX = newDivComplex.newElement.style.left;
+	positionX = parseInt(positionX) - 10;
+	newDivComplex.newElement.style.left = positionX + "px";
+};
+
 const DomElement = function (selector, height, width, bg, fontSize, text) {
 	this.selector = selector;
 	this.height = height;
@@ -9,11 +50,12 @@ const DomElement = function (selector, height, width, bg, fontSize, text) {
 	this.text = text;
 	this.newElement;
 
-	this.createElement = function () {
+	this.createElement = () => {
 		if (this.selector[0] === ".") {
 			this.newElement = document.createElement("div");
 			this.newElement.classList.add(this.selector);
 			this.newElement.style.cssText = `height: ${this.height}; width: ${this.width}; background: ${this.bg}; font-size: ${this.fontSize};`;
+			this.newElement.style.position = "absolute";
 			this.newElement.textContent = this.text;
 			document.body.append(this.newElement);
 			// console.log(this.newElement);
@@ -28,8 +70,14 @@ const DomElement = function (selector, height, width, bg, fontSize, text) {
 	};
 };
 
-const newDiv1 = new DomElement(".class", "100px", "100px", "green", "18px", "asdas");
-newDiv1.createElement();
+// const newDiv1 = new DomElement(".class", "100px", "100px", "green", "18px", "asdas");
+// newDiv1.createElement();
 
-const newDiv2 = new DomElement("#id", "50px", "200px", "blue", "24px", "was ist das");
-newDiv2.createElement();
+// const newDiv2 = new DomElement("#id", "50px", "200px", "blue", "24px", "was ist das");
+// newDiv2.createElement();
+
+const newDivComplex = new DomElement(".class", "100px", "100px", "MediumAquamarine", "20px", "Object");
+newDivComplex.createElement();
+
+newDivComplex.newElement.style.top = "0px";
+newDivComplex.newElement.style.left = "0px";
