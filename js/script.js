4feffda6c85e837f8f3e201c3a4bf5d5a45de3e3
MYuPopov <beemax@bk.ru>
2021-12-16 18:12:16 +0300

    Обязательное задание Урока 15

--- js/script.js
+++ js/script.js
@@ -1,9 +1,5 @@
 "use strict";
 
-// console.log("Файл script подгрузился");
-
-// console.log(document.body);
-
 const DomElement = function (selector, height, width, bg, fontSize, text) {
 	this.selector = selector;
 	this.height = height;
@@ -20,22 +16,20 @@ const DomElement = function (selector, height, width, bg, fontSize, text) {
 			this.newElement.style.cssText = `height: ${this.height}; width: ${this.width}; background: ${this.bg}; font-size: ${this.fontSize};`;
 			this.newElement.textContent = this.text;
 			document.body.append(this.newElement);
-			console.log(this.newElement);
+			// console.log(this.newElement);
 		} else if (this.selector[0] === "#") {
 			this.newElement = document.createElement("p");
 			this.newElement.classList.add(this.selector);
 			this.newElement.style.cssText = `height: ${this.height}; width: ${this.width}; background: ${this.bg}; font-size: ${this.fontSize};`;
 			this.newElement.textContent = this.text;
 			document.body.append(this.newElement);
-			console.log(this.newElement);
-		} /* else {
-			alert("Введите selector, начинающийся либо с . либо с #");
-		} */
+			// console.log(this.newElement);
+		}
 	};
 };
 
 const newDiv1 = new DomElement(".class", "100px", "100px", "green", "18px", "asdas");
 newDiv1.createElement();
 
-const newDiv2 = new DomElement("#id", "50px", "200px", "blue", "24px", "wasistdas");
+const newDiv2 = new DomElement("#id", "50px", "200px", "blue", "24px", "was ist das");
 newDiv2.createElement();
